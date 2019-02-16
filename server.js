0dc8b77260e86d9a8018fd5cc17c69a1ec23a8c0
Gamn69 <druckerg@gmail.com>
2019-02-16 13:17:15 -0800

    Added launchRequestHandler

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const Alexa = require('ask-sdk-core');
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+      const speechText = 'What do you want?';
+  
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .withSimpleCard('WHY DID YOU WAKE ME WITHOUT TELLING ME WHAT TO DO?', speechText)
+        .getResponse();
+    }
+  };
