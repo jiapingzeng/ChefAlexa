4a19a87a24f94e9877095ae568a589c8dff5085d
Gamn69 <druckerg@gmail.com>
2019-02-16 14:13:57 -0800

    Added some handlers

--- server.js
+++ server.js
@@ -1,25 +1,73 @@
 const https = require('https')
-const port = 3000
+const port = 3139
 const server = https.createServer(requestHandler)
 const Alexa = require('ask-sdk-core');
+ 
+let skill;
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     handle(handlerInput) {
-      const speechText = 'What do you want?';
-  
+        const speechText = 'Hello, I am your personal assistant PageBot please let me send your location to Amazon before we begin.';
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Location Sent.', speechText)
+            .getResponse();
+    }
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+      const speechText = 'You can ask me what you can make if you have certain ingredients';
       return handlerInput.responseBuilder
         .speak(speechText)
         .reprompt(speechText)
-        .withSimpleCard('WHY DID YOU WAKE ME WITHOUT TELLING ME WHAT TO DO?', speechText)
+        .withSimpleCard('Hello World', speechText)
         .getResponse();
     }
   };
 
-  server.listen(port, (err) => {
-    if (err) {
-      return console.log('Error: ', err)
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
+
+        return handlerInput.responseBuilder
+            .speak('Sorry, I can\'t understand what it is your are trying to say. Can you please restate?')
+            .reprompt('Sorry, I can\'t understand what it is your are trying to say. Can you please restate?')
+            .getResponse();
+    },
+};
+
+exports.handler = async function (event, context) {
+    if (!skill) {
+        skill = Alexa.SkillBuilders.custom()
+            .addRequestHandlers(
+                LaunchRequestHandler,
+                HelpIntentHandler,
+                CancelAndStopIntentHandler,
+                SessionEndedRequestHandler,
+            )
+            .addErrorHandlers(ErrorHandler)
+            .create();
     }
-  })
+
+    const response = await skill.invoke(event, context);
+    return response;
+};
+
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('You have a error: ', err)
+    }
+})
