eda7a9dcf30e30b23e5ec059ef13123629f13ef0
Gamn69 <druckerg@gmail.com>
2019-02-16 13:31:15 -0800

    Merged previous file

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
+const https = require('https')
+const port = 3000
+const server = https.createServer(requestHandler)
+const Alexa = require('ask-sdk-core');
 
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+      const speechText = 'What do you want?';
+  
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .withSimpleCard('WHY DID YOU WAKE ME WITHOUT TELLING ME WHAT TO DO?', speechText)
+        .getResponse();
+    }
+  };
 
-console.log('hello')
+  server.listen(port, (err) => {
+    if (err) {
+      return console.log('Error: ', err)
+    }
+  })
