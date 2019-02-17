08ef7198238422b2c0fb93c5ee9a79a4694ffe23
Gamn69 <druckerg@gmail.com>
2019-02-17 01:03:38 -0800

    Hmmm

--- alexa/index.js
+++ alexa/index.js
@@ -1,6 +1,27 @@
 'use strict';
 const Alexa = require('alexa-sdk');
 
+//JP Recipe Stuff
+const appId = 'a593916b'
+const appKey = '5e107e6eccb15211d97ac9cc54c287de'
+const appUri = 'https://api.edamam.com/search'
+
+function getRecipe (keywords) {
+  
+  var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", 'https://chefalexa.herokuapp.com/recipe?ingredinents?' + keywords.split(' '), true ); // false for synchronous request
+    xmlHttp.send(null);
+    return JSON.parse(xhr.responseText)[0];
+}
+
+function getIngredients (r) {
+  var ingredients = ""
+  for (i = 0; i < 5 && i < r.ingredientLines.length; i++) {
+    ingredients += r.ingredientLines[i] + ", "
+  }
+  return ingredients
+}
+
 const handlers = {
   'LaunchRequest': function () {
     const responseText = 'Hello I am Chef Alexa! Tell me what ingredients you have on hand and I will tell you what you can make with them!'
@@ -27,20 +48,21 @@ const handlers = {
   'AMAZON.FallbackIntent': function () {
     const responseText = 'I am sorry, but I do not understand what you are trying to say. Can you please rephrase your query?'
     this.response.speak(responseText);
-    this.response.cardRenderer("Chef Alexa: I don't understand", responseText);
     this.emit(':responseReady')
   },
-  'RecipeIntent' : function (input) {
+  'RecipeIntent': function (input) {
     var foodList = this.event.request.intent.slots.foodA.value;
-    this.response.speak('You have' + foodList);
-    this.response.cardRenderer('Chef Alexa: You Have: ' + foodList);
+    const recipe = getRecipe(foodList)
+    const ingredients = getIngredients(recipe)
+    this.response.speak(`Would you like to try` + recipe.label + '?' `Here are the ingredients you need:` + ingredients);
+    this.response.cardRenderer(`Chef Alexa: ${recipe.label} `, foodList);
     this.emit(':responseReady')
   },
-  'easterEggIntent' : function() {
+  'easterEggIntent': function () {
     this.response.speak("RUN <audio src='soundbank://soundlibrary/scifi/amzn_sfx_scifi_alarm_02'/><audio src='soundbank://soundlibrary/scifi/amzn_sfx_scifi_alarm_03'/><audio src='soundbank://soundlibrary/scifi/amzn_sfx_scifi_alarm_03'/>")
     this.emit(':responseReady')
   },
-  'whoIsTheChefIntent' : function () {
+  'whoIsTheChefIntent': function () {
     var responseText = 'The chef is you, Alexa, and the internet. The chef can find any recipe with any ingredients.'
     this.response.speak(responseText);
     this.response.cardRenderer('Chef Alexa: The Chef Is: ' + responseText);
