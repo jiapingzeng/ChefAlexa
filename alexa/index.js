01546174cc4e3e2a12e9760edc3353f3cbafd446
Gamn69 <druckerg@gmail.com>
2019-02-17 01:54:14 -0800

    Added GET requests

--- alexa/index.js
+++ alexa/index.js
@@ -1,5 +1,33 @@
 'use strict';
 const Alexa = require('alexa-sdk');
+const request = require('sync-request')
+
+const appId = 'a593916b'
+const appKey = '5e107e6eccb15211d97ac9cc54c287de'
+const appUri = 'https://api.edamam.com/search'
+
+var getRecipe = (keywords) => {
+  var res = request('GET', appUri, {
+    qs: {
+      app_id: appId,
+      app_key: appKey,
+      q: keywords,
+      to: 10
+    }
+  })
+  var body = JSON.parse(res.getBody())
+  var recipes = body.hits
+  return recipes
+}
+
+var getIngredients = (r) => {
+  var ingredients = ""
+  for (i = 0; i < 5 && i < r.ingredientLines.length; i++) {
+    ingredients += r.ingredientLines[i] + ", "
+  }
+  return ingredients
+}
+
 
 const handlers = {
   'LaunchRequest': function () {
@@ -25,13 +53,22 @@ const handlers = {
     this.emit(':responseReady')
   },
   'AMAZON.FallbackIntent': function () {
-    const responseText = 'I am sorry, but I do not understand what you are trying to say. Can you please rephrase your quary?'
+    const responseText = 'I am sorry, but I do not understand what you are trying to say. Can you please rephrase your query?'
     this.response.speak(responseText);
     this.response.cardRenderer("Chef Alexa: I don't understand", responseText);
     this.emit(':responseReady')
   },
   'easterEggIntent': function () {
     this.response.speak("RUN <audio src='soundbank://soundlibrary/scifi/amzn_sfx_scifi_alarm_02'/><audio src='soundbank://soundlibrary/scifi/amzn_sfx_scifi_alarm_03'/><audio src='soundbank://soundlibrary/scifi/amzn_sfx_scifi_alarm_03'/>")
+    this.emit(':responseReady')
+  },
+  'RecipeIntent': function () {
+    var FoodA = this.event.request.intent.slots.foodA.value;
+    const recipe = getRecipe(FoodA)[0].recipe
+    //const ingredients = getIngredients(recipe)
+    this.response.speak(`Would you like to try ${recipe.label}?`)
+    this.response.cardRenderer(`Chef Alexa: ${recipe.label}` , `Would you like to try ${recipe.label}?`);
+
     this.emit(':responseReady')
   },
   'whoIsTheChefIntent': function () {
