51d237d724091f1a301ac78ea9d7cfb67f41ee3f
aaron <aahuang@berkeley.edu>
2019-02-16 16:15:40 -0800

    add test route

--- frontend/web/server.js
+++ frontend/web/server.js
@@ -4,6 +4,9 @@ const fs = require('fs')
 app.use(express.json())
 app.use(express.static('./views'))
 
+app.get('/test', (req, res)=>{
+	res.send({hello: 'world'}
+	)}	)
 app.listen(process.env.PORT || 3000, function(){
 	console.log(`Now listening on ${process.env.PORT || 3000}`)})
 
