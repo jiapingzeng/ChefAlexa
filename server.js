acfb7b5c4e5401b9222d0c88c704db3bc55d7035
Gamn69 <druckerg@gmail.com>
2019-02-17 00:23:26 -0800

    Added /splash?

--- server.js
+++ server.js
@@ -12,8 +12,14 @@ app.get('/test', (req, res) => {
 	res.json({ hello: 'world' }
 	)
 })
-app.get('/idk', (req, res) => {
-	res.json(example.hits)
+app.get('/splash', (req, res) => {
+	if (req.secure) {
+		res.json(splash.html)
+	} else {
+		// request was via http, so redirect to https
+		res.redirect('https://' + req.headers.host + req.url);
+	}
+
 })
 app.get('/recipe-list', (req, res) => {
 	console.log(JSON.parse(req.query.ingredients))
