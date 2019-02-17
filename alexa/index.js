bd551795df4dd659bc1b70497a18af0768f5d031
Gamn69 <druckerg@gmail.com>
2019-02-17 02:20:32 -0800

    Now with ingredints

--- alexa/index.js
+++ alexa/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const Alexa = require('alexa-sdk');
 const request = require('sync-request')
+var random = require('math-random')
 
 const appId = 'a593916b'
 const appKey = '5e107e6eccb15211d97ac9cc54c287de'
@@ -52,12 +53,6 @@ const handlers = {
     this.response.speak(responseText);
     this.emit(':responseReady')
   },
-  'AMAZON.FallbackIntent': function () {
-    const responseText = 'I am sorry, but I do not understand what you are trying to say. Can you please rephrase your query?'
-    this.response.speak(responseText);
-    this.response.cardRenderer("Chef Alexa: I don't understand", responseText);
-    this.emit(':responseReady')
-  },
   'easterEggIntent': function () {
     this.response.speak("RUN <audio src='soundbank://soundlibrary/scifi/amzn_sfx_scifi_alarm_02'/><audio src='soundbank://soundlibrary/scifi/amzn_sfx_scifi_alarm_03'/><audio src='soundbank://soundlibrary/scifi/amzn_sfx_scifi_alarm_03'/>")
     this.emit(':responseReady')
@@ -65,10 +60,16 @@ const handlers = {
   'RecipeIntent': function () {
     var FoodA = this.event.request.intent.slots.foodA.value;
     const recipe = getRecipe(FoodA)[0].recipe
-    var ingredients = recipe.ingredientLines
-    this.response.speak(`Would you like to try ${recipe.label}? It requires ${ingredients}`)
-    this.response.cardRenderer(`Chef Alexa: ${recipe.label}` , `Would you like to try ${recipe.label}?`);
+    var ingredients = recipe.ingredientLines[0] + ', ' + recipe.ingredientLines[1]
+    this.response.speak(`Would you like to try ${recipe.label}? It requires ${ingredients}. For more information on this recipe and other checkout the Chef Alexa webapp on your phone.`)
+    this.response.cardRenderer(`Chef Alexa: ${recipe.label}`, `Would you like to try ${recipe.label}?`);
 
+    this.emit(':responseReady')
+  },
+  'AMAZON.FallbackIntent': function () {
+    const responseText = 'I am sorry, but I do not understand what you are trying to say. Can you please rephrase your query?'
+    this.response.speak(responseText);
+    this.response.cardRenderer("Chef Alexa: I don't understand", responseText);
     this.emit(':responseReady')
   },
   'whoIsTheChefIntent': function () {
